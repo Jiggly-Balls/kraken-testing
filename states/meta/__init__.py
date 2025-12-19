8d748478b7d5b9bd9720795029127e366af0db15
Jiggly Balls <jigglyballs9000@gmail.com>
2025-12-19 20:27:54 +0530

    Added BaseManager & LoaderState to meta __init__

--- states/meta/__init__.py
+++ states/meta/__init__.py
@@ -1,7 +1,11 @@
-from states.meta.base import BaseState
+from states.meta.base_manager import BaseManager
+from states.meta.base_state import BaseState
+from states.meta.loader import LoaderState
 from states.meta.state_enums import StateEnum
 
 __all__ = (
+    "BaseManager",
     "BaseState",
+    "LoaderState",
     "StateEnum",
 )
