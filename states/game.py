6ad7a931bfca51b58a226b1e17ca52f7e807537e
Jiggly Balls <jigglyballs9000@gmail.com>
2025-12-20 18:39:37 +0530

    Added player

--- states/game.py
+++ states/game.py
@@ -1,4 +1,29 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pykraken as kn
+
+from entities.player import PlayerStates
 from states.meta import BaseState, StateEnum
 
+if TYPE_CHECKING:
+    from pykraken import Event
 
-class GameState(BaseState, state_name=StateEnum.GAME): ...
+
+class GameState(BaseState, state_name=StateEnum.GAME):
+    def on_enter(self, previous_state: None | BaseState) -> None:
+        self.player.animation.set(PlayerStates.IDLE)
+        self.player.animation.play(PlayerStates.IDLE)
+
+    def process_event(self, event: Event, dt: float) -> None:
+        self.player.process_event(event)
+
+    def process_update(self, dt: float) -> None:
+        self.player.process_update(dt)
+
+        kn.renderer.draw(
+            self.player.animation.texture,
+            kn.Transform(self.player.position),
+            src=self.player.animation.clip,
+        )
--- states/meta/base_state.py
+++ states/meta/base_state.py
@@ -3,10 +3,12 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from game_state import State
+from game_state.utils import MISSING
 
 if TYPE_CHECKING:
     from pykraken import Event
 
+    from entities.player import Player
     from states.meta.base_manager import BaseManager
 
 
@@ -15,8 +17,9 @@ __all__ = ("BaseState",)
 
 class BaseState(State["BaseState"]):
     manager: BaseManager  # pyright: ignore[reportIncompatibleVariableOverride]
+    player: Player = MISSING
 
     def process_update(self, dt: float) -> None:  # pyright: ignore[reportIncompatibleMethodOverride]
         ...
 
-    def process_event(self, event: Event) -> None: ...
+    def process_event(self, event: Event, dt: float) -> None: ...  # pyright: ignore[reportIncompatibleMethodOverride]
